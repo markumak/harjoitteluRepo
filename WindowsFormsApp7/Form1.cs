fae1a5a71b71cec6884f6099800b4d36a2dec5bf
markumak <73663778+markumak@users.noreply.github.com>
2021-04-13 17:12:26 +0300

    Button1 klikkieventtiä muutettu

--- WindowsFormsApp7/Form1.cs
+++ WindowsFormsApp7/Form1.cs
@@ -19,13 +19,13 @@ namespace WindowsFormsApp7
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if (button1.BackColor == Color.Cyan)
+            if (button1.BackColor == Color.Red)
             {
                 button1.BackColor = Color.Green;
             }
             else
             {
-                button1.BackColor = Color.Cyan;
+                button1.BackColor = Color.Red;
             }
 
 
